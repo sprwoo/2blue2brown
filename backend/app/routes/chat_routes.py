68e4de0b9c72e5455f66d1d1330d4857789964e7
Hetul Patel <patehetu239@gmail.com>
2025-03-30 20:12:01 -0400

    WIP

--- backend/app/routes/chat_routes.py
+++ backend/app/routes/chat_routes.py
@@ -46,34 +46,34 @@ def handle_chat():
     print("session_id", chat_session_id)
     print("user_input", message)
 
-    # image_summary = None
-    # if "image" in request.files:
-    #     image_file = request.files["image"]
-    #     if image_file.filename != "":
-    #         chunky = Chunky()
-    #         file_bytes = image_file.read()
-    #         encoded_image = base64.b64encode(file_bytes).decode('utf-8')
-    #         image_summary = chunky.advanced_image_handling(user_input, encoded_image)
-    #         user_input+=f" The user also uploaded an image with these being the contents of the image: \n\n {image_summary}"
+    image_summary = None
+    if "image" in request.files:
+        image_file = request.files["image"]
+        if image_file.filename != "":
+            chunky = Chunky()
+            file_bytes = image_file.read()
+            encoded_image = base64.b64encode(file_bytes).decode('utf-8')
+            image_summary = chunky.advanced_image_handling(user_input, encoded_image)
+            user_input+=f" The user also uploaded an image with these being the contents of the image: \n\n {image_summary}"
         
-    # graph = build_graph()
+    graph = build_graph()
     
-    # state = {
-    #     "user_input": user_input,
-    #     "session_id": chat_session_id,
-    # }
-    # result = graph.invoke(state)
+    state = {
+        "user_input": user_input,
+        "session_id": chat_session_id,
+    }
+    result = graph.invoke(state)
     
     # we'll now save the user and ai messages
-    # chat_session_id = session_id
-    # sender = "user"
-    # message = user_input
+    chat_session_id = session_id
+    sender = "user"
+    message = user_input
     post_status = post_message(sender, message, chat_session_id)
     
-    # sender = "ai"
-    # message = result.get("chat_response")
-    # manim_code = "\n".join(result.get("code_chunks", [])) or None
-    # post_status = post_message(sender, message, chat_session_id, manim_code=manim_code, image_summary=image_summary)
+    sender = "ai"
+    message = result.get("chat_response")
+    manim_code = "\n".join(result.get("code_chunks", [])) or None
+    post_status = post_message(sender, message, chat_session_id, manim_code=manim_code, image_summary=image_summary)
     
     return jsonify(post_status), 200
         
